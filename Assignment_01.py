5b2986b46849f6c4e154b9d27638a525f0d5f415
Martijn <martijn.ab@home.nl>
2019-12-27 15:14:12 +0100

    gaat goed fout

--- Assignment_01.py
+++ Assignment_01.py
@@ -32,6 +32,7 @@ def fetch_tables(html):
     """
     soup = BeautifulSoup(html, 'html.parser')
     tables = soup.findAll(attrs={'class': re.compile(r".*\bwikitable\b.*")})
+    # tables = soup.findAll(attrs={'class': re.compile(r".*\btable\b.*")})
     print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     print(tables)
     print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
@@ -41,6 +42,8 @@ def fetch_tables(html):
 def table_df(table):
     """parses the html table to a pandas dataframe"""
     # fetch dimensions
+    print(help(table))
+
     l = len(table.find_all('tr'))
     w = len(table.find_all('tr')[0].find_all('td'))
     matrix = [['' for i in range(0, w)] for i in range(0, l)]
@@ -51,14 +54,27 @@ def table_df(table):
     # put in df making first row the header
     df = pd.DataFrame(matrix[1:], columns=matrix[0])
     return df
+    # print(help(table))
+
+    # l = len(table.findall('tr'))
+    # w = len(table.findall('tr')[0].find_all('td'))
+    # matrix = [['' for i in range(0, w)] for i in range(0, l)]
+    # # fetch content
+    # for i, row in enumerate(table.findall('tr')):
+    #     for j, column in enumerate(row.find_all('td')):
+    #         matrix[i][j] = column.get_text().strip()
+    # # put in df making first row the header
+    # df = pd.DataFrame(matrix[1:], columns=matrix[0])
+    # return df
 
 
 def main():
-    # html = open_url('https://en.wikipedia.org/wiki/Tilburg_Trappers')
-    html = open_url('https://www.knmi.nl/nederland-nu/seismologie/aardbevingen')
+    html = open_url('https://en.wikipedia.org/wiki/Tilburg_Trappers')
+    # html = open_url('https://www.knmi.nl/nederland-nu/seismologie/aardbevingen')
     t = fetch_tables(html)
-    # df = table_df(t)
-    # print(df)
+    df = table_df(t)
+    print(df)
+    print("hallo")
     # return 0
 
 
