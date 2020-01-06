491a58b29220308aab0a6f7584c8976cf145c0d0
martijnab <martijn.ab@home.nl>
2020-01-06 13:52:35 +0100

    fout ondekt

--- Assignment_01.py
+++ Assignment_01.py
@@ -31,26 +31,41 @@ def fetch_tables(html):
         more readable. input: html, output: tables
     """
     soup = BeautifulSoup(html, 'html.parser')
-    tables = soup.findAll(attrs={'class': re.compile(r".*\bwikitable\b.*")})
-    # tables = soup.findAll(attrs={'class': re.compile(r".*\btable\b.*")})
+    # tables = soup.findAll(attrs={'class': re.compile(r".*\bwikitable\b.*")})
+    tables = soup.findAll(attrs={'class': re.compile(r".*\btable\b.*")})
     print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     print(tables)
     print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    # return tables[0]
+    return tables[0]
 
 
 def table_df(table):
     """parses the html table to a pandas dataframe"""
     # fetch dimensions
-    print(help(table))
+    # print(help(table))
 
     l = len(table.find_all('tr'))
+    # print(l)
+    # input()
     w = len(table.find_all('tr')[0].find_all('td'))
+    # print(w)
+    # input()
     matrix = [['' for i in range(0, w)] for i in range(0, l)]
+    # print(matrix)
+    # input()
     # fetch content
     for i, row in enumerate(table.find_all('tr')):
+        print(i)
+        print(row)
+        input()
+        print(row.find_all('td'))
         for j, column in enumerate(row.find_all('td')):
+            print("b")
+            print(j)
+            print(column)
+            input()
             matrix[i][j] = column.get_text().strip()
+            print(matrix)
     # put in df making first row the header
     df = pd.DataFrame(matrix[1:], columns=matrix[0])
     return df
@@ -69,8 +84,8 @@ def table_df(table):
 
 
 def main():
-    html = open_url('https://en.wikipedia.org/wiki/Tilburg_Trappers')
-    # html = open_url('https://www.knmi.nl/nederland-nu/seismologie/aardbevingen')
+    # html = open_url('https://en.wikipedia.org/wiki/Tilburg_Trappers')
+    html = open_url('https://www.knmi.nl/nederland-nu/seismologie/aardbevingen')
     t = fetch_tables(html)
     df = table_df(t)
     print(df)
